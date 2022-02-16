747de1703efc6ebc7710f1105e8b559adac04771
chinosk <2248589280@qq.com>
2022-02-16 15:19:27 +0800

    更新文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
     title: 'chieri bot docs',
     description: 'cheiri bot 频道文档',
     host: "0.0.0.0",
-    port: 8080,
+    port: 18794,
 
     head: [ 
         ['link', { rel: 'icon', href: '/g.jpg' }],
@@ -12,6 +12,7 @@ module.exports = {
         lineNumbers: false  // 代码块行号
       },
       themeConfig: {
+        logo: "/logo.png",
         nav:[ // 导航栏配置
           {text: "爱发电", link: "https://afdian.net/@chieri"},
           {text: "哔哩哔哩", link: "https://space.bilibili.com/697847106"}
@@ -27,9 +28,12 @@ module.exports = {
                 "pjsk",
                 "sdvx",
                 "ycm",
-                "others"
+                "others",
+                "about"
             ]
         },
+        repo: 'chieri-bot/guild-bot-docs',
+        repoLabel: 'GitHub',
 
         sidebarDepth: 2, // 侧边栏显示2级
       }
---
+++ docs/about.md
@@ -0,0 +1,29 @@
+# 关于
+
+- 开发者: 'sunset, QQ: `2248589280`
+- 交流群: `711422712`(音游/反馈群), `478547016`(反馈群)
+- chieri bot 频道: [点击加入](https://qun.qq.com/qqweb/qunpro/share?_wv=3&_wwv=128&inviteCode=PMg2z&from=246610&biz=ka)
+
+
+
+## 部分数据来源
+
+- `ArcaeaUnlimitedAPI` - Arcaea
+- `Arcaea Limited API`(*[Lowiro](https://lowiro.com/)) - Arcaea
+- [舞萌 DX 查分器](https://www.diving-fish.com/maimaidx/prober/) - maimai
+- [Project Sekai Profile](https://profile.pjsekai.moe/) - Project Sekai
+- [sekai.best](https://sekai.best/) - Project Sekai
+- `Arcana` - SDVX成绩查询
+
+
+
+## 参与开发
+
+- `Hidden is Fun` - maimai模块维护
+- `SoreHait` - 提供部分多语言支持
+
+
+
+## 特别鸣谢
+
+- 您
